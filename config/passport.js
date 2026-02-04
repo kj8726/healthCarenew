53258db0e0d70fdc94948670c8f6324b9813fbf0
kj8726 <kunal.jadhav8726@gmail.com>
2026-02-04 22:55:43 +0530

    at last final

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 
       process.env.NODE_ENV === "production"
-          ? "https://healthcare-9anf.onrender.com/auth/google/callback"
+          ? "https://healthcarenew-q46s.onrender.com/auth/google/callback"
           : "http://localhost:3000/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
